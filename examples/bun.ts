b7f9edcdb7458a6850e8fa781409fc3293e1c80e
utftufutukgyftryidytftuv <overby999@gmail.com>
2024-09-13 19:24:47 +0300

    13.09.24 19:24

--- examples/bun.ts
+++ examples/bun.ts
@@ -17,16 +17,24 @@ h11.get('/hello/**', ({ params }) => {
 
 const bunProvider = bunProviderFactory({ h11 });
 
-const server = Bun.serve({
-  // hostname: 'hello.world',
-  port: '443',
-  async fetch(req, server) {
-    console.log('-----', 'req.url', req.url);
-    // console.log('-----', 'args', args);
-    // req.body.
-    const res = await bunProvider(req, server);
-    return res;
+// const server = Bun.serve({
+//   // hostname: 'hello.world',
+//   unix: '\0benchmark-h11-bun',
+//   // port: '443',
+//   async fetch(req, server) {
+//     console.log('-----', 'req.url', req.url);
+//     // console.log('-----', 'args', args);
+//     // req.body.
+//     const res = await bunProvider(req, server);
+//     return res;
+//   },
+// });
+
+// console.log('-----', 'server', server.url);
+
+Bun.serve({
+  unix: '/tmp/my-socket.sock', // abstract namespace socket
+  fetch(req) {
+    return new Response(`404!`);
   },
 });
-
-console.log('-----', 'server', server.url);
