6fa5755d08ce4e583f1aedac2accde86463819c8
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-10-18 16:02:05 +0300

    18.10.25 16:02

--- packages/h11-node/dist/types/fs.node.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FsApi } from 'h11';
-export declare const copyFiles: (from: string, to: string) => Promise<void>;
-export declare const mkdirNode: (path: string) => Promise<void>;
-export declare const rmNode: (path: string) => Promise<void>;
-export declare const fsApiNode: FsApi;
--- packages/h11-node/dist/types/provider.node.d.ts
+++
@@ -1,9 +0,0 @@
-import type { H11 } from 'h11';
-import type { IncomingMessage, ServerResponse } from 'node:http';
-export declare const createNodeProvider: ({ h11 }: {
-    h11: H11;
-}) => ({ req, res, origin, }: {
-    req: IncomingMessage;
-    res: ServerResponse<IncomingMessage>;
-    origin: string;
-}) => Promise<void>;
